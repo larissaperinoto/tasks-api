f1980be349ab80cca4e9617622542e8885b3b581
Larissa Perinoto <perinotolarissa@gmail.com>
2023-10-24 23:11:42 -0300

    feat: insere rota de criação de task

--- app/Controllers/Http/TasksController.ts
+++ app/Controllers/Http/TasksController.ts
@@ -2,6 +2,18 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Task from 'App/Models/Task'
 
 export default class TasksController {
+
+  public async store({ request, response }: HttpContextContract) {
+
+    const body = request.body()
+
+    response.status(201)
+
+    const res = await Task.create(body)
+
+    return res
+  }
+
   public async index() {
 
     const tasks = await Task.all()
