5660c50a37306fbe2558550c560b103c0ae6207b
Larissa Perinoto <perinotolarissa@gmail.com>
2023-10-24 22:24:37 -0300

    feat: insere migration da tabela tasks

---
+++ database/migrations/1698195640579_tasks.ts
@@ -0,0 +1,20 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'tasks'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('description')
+      table.string('title').notNullable()
+      table.string('status').notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
